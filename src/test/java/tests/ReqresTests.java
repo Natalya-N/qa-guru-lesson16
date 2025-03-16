6b2b48d0dcbd80b06d7b9884737e90926dc688e3
Nechaeva Natalya <nechaevanatascha@yandex.ru>
2025-03-16 18:30:25 +0300

    Added specs

--- src/test/java/tests/ReqresTests.java
+++ src/test/java/tests/ReqresTests.java
@@ -14,6 +14,7 @@ import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static specs.TestSpec.*;
 
 @Tag("simple")
 public class ReqresTests {
@@ -32,7 +33,7 @@ public class ReqresTests {
         request.setJob("QA");
 
         UserResponseModel response = step("Создать нового пользователя, указать имя и работу",
-                () -> given()
+                () -> given(requestSpec)
                         .body(request)
                         .contentType(JSON)
                         .log().uri()
@@ -41,9 +42,7 @@ public class ReqresTests {
                         .when()
                         .post("/users")
                         .then()
-                        .log().status()
-                        .log().body()
-                        .statusCode(201)
+                        .spec(statusCode201Spec)
                         .extract().as(UserResponseModel.class));
 
         step("Проверить, что в ответе вернулись указанные в запросе данные", () -> {
@@ -58,15 +57,13 @@ public class ReqresTests {
     @DisplayName("Проверить успешное получение пользователя")
     void successfulGetUserTest() {
         UserFullResponseModel response = step("Получить пользователя по id",
-                () -> given()
+                () -> given(requestSpec)
                         .contentType(JSON)
                         .log().uri()
                         .when()
                         .get("/users/2")
                         .then()
-                        .log().status()
-                        .log().body()
-                        .statusCode(200)
+                        .spec(statusCode200Spec)
                         .extract().as(UserFullResponseModel.class));
 
         step("Проверить, что в ответе вернулись данные о пользователе", () -> {
@@ -88,7 +85,7 @@ public class ReqresTests {
         request.setJob("AQA");
 
         UserResponseModel response = step("Изменить данные о пользователе",
-                () -> given()
+                () -> given(requestSpec)
                         .body(request)
                         .contentType(JSON)
                         .log().uri()
@@ -97,9 +94,7 @@ public class ReqresTests {
                         .when()
                         .put("/users/95")
                         .then()
-                        .log().status()
-                        .log().body()
-                        .statusCode(200)
+                        .spec(statusCode200Spec)
                         .extract().as(UserResponseModel.class));
 
         step("Проверить, что в ответе вернулись обновленные данные", () -> {
@@ -113,30 +108,26 @@ public class ReqresTests {
     @DisplayName("Проверить успешное удаление пользователя")
     void successfulDeleteUserTest() {
         step("Удалить пользователя по id",
-                () -> given()
+                () -> given(requestSpec)
                         .contentType(JSON)
                         .log().uri()
                         .when()
                         .delete("/users/2")
                         .then()
-                        .log().status()
-                        .log().body()
-                        .statusCode(204));
+                        .spec(statusCode204Spec));
     }
 
     @Test
     @DisplayName("Проверить, что для несуществующего пользователя возвращается код 404")
     void unsuccessfulUserNotFoundTest() {
         step("Получить пользователя по несуществующему id",
-                () -> given()
+                () -> given(requestSpec)
                         .contentType(JSON)
                         .log().uri()
                         .when()
                         .get("/users/23")
                         .then()
-                        .log().status()
-                        .log().body()
-                        .statusCode(404));
+                        .spec(statusCode404Spec));
     }
 
 }
